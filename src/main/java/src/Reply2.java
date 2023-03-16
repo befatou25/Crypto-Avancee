c6ecbddb204eb7220985354c7727fa52ebf9b8a9
Khawlaaitka <95345774+Khawlaaitka@users.noreply.github.com>
2023-03-16 14:07:23 +0100

    Update Reply2.java
    La méthode qui permet de choisir le num de mail a été ajouté dans la classe Reply2

--- src/main/java/src/Reply2.java
+++ src/main/java/src/Reply2.java
@@ -34,6 +34,61 @@ public class Reply2 {
         catch (MessagingException e) {e.printStackTrace();}
 
     }
+    
+    public static void NumMail(String username, String password) {
+
+        try {
+            Properties properties = new Properties();
+            // server setting (it can be pop3 too
+            properties.put("mail.imap.host", "outlook.office365.com");
+            properties.put("mail.imap.port", "993");
+            properties.setProperty("mail.imap.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+            properties.setProperty("mail.imap.socketFactory.fallback", "false");
+            properties.setProperty("mail.imap.socketFactory.port", "993");
+            Session session = Session.getDefaultInstance(properties);
+            Store store = session.getStore("imap");
+            store.connect(username, password);
+            Folder folderInbox = store.getFolder("INBOX");
+            if (!folderInbox.isOpen()) {
+                folderInbox.open(Folder.READ_ONLY);
+            }
+            Message[] arrayMessages = folderInbox.getMessages();
+            List<Integer> nbrList = new ArrayList<Integer>();
+            for (int i = 0; i < arrayMessages.length; i++) {
+                int nbr = i + 1;
+                nbrList.add(nbr);
+                System.out.println("*************Voici le numero de mail : " + nbr);
+            }
+            System.out.println(nbrList);
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Veuillez choisir le numero du mail auquel vous voulez répondre : ");
+            int numMail = sc.nextInt();
+            System.out.println("voici le num entré : " + numMail);
+            sc.close();
+            if (nbrList.contains(numMail)) {
+                try{
+                    folderInbox.getMessage(numMail - 1);
+                    Scanner sc1 = new Scanner(System.in);
+                    System.out.println("Veuillez entrez l'adresse mail de réception  de la réponse : ");
+                    String destination = sc1.next();
+                    Reply2(username,password,destination, numMail);
+                    sc1.close();
+
+
+                }catch (MessagingException ex) {
+                    System.out.println("Could not connect to the message store");
+                    ex.printStackTrace();
+                }
+            }
+
+            folderInbox.close(false); // ferme la boîte aux lettres sans supprimer les messages
+            store.close(); // ferme la connexion à la boîte aux lettres
+        } catch (MessagingException ex) {
+            System.out.println("Could not connect to the message store");
+            ex.printStackTrace();
+        }
+
+    }
 
 
     }
